7d171bc6febbcf500950f6584bc19c956f6ed41a
Rakesh6720 <rbaruah@gmail.com>
2022-03-19 14:59:20 -1000

    add store context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,16 @@ import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { StoreProvider } from "./app/context/StoreContext";
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
