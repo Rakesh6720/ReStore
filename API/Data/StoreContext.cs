3313ccc132e5a8292cf4652da04e432b485ea5ec
Rakesh Baruah <rbaruah@gmail.com>
2022-03-30 03:34:40 -0500

    add user and identity db context

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,14 +1,29 @@
 using Api.Entities;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace API.Data {
-    public class StoreContext : DbContext {
+    public class StoreContext : IdentityDbContext<User> {
         public StoreContext(DbContextOptions options) : base(options) {
 
         }
 
         public DbSet<Product> Products {get; set;}
         public DbSet<Basket> Baskets {get; set;}
+
+        // add some data to the database
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>()
+                .HasData(
+                    new IdentityRole{Name = "Member", NormalizedName = "MEMBER"},
+                    new IdentityRole{Name = "Admin", NormalizedName = "ADMIN"}
+                );
+        }
 
     }
 }
